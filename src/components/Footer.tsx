72a3bd4721c05da0919758308da56f2389392036
Makene Neto <makeneto17@gmail.com>
2026-08-20 14:43:18 +0100

    feat: develop reusable Footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,24 @@
+import Link from "next/link"
+import { Button } from "./ui/button"
+
+export default function Footer({ variant }: { variant: "mobile" | "desktop" }) {
+  const now = new Date()
+  const currentYear = now.getFullYear()
+
+  return (
+    <p
+      className={`
+        text-center text-xs font-medium text-neutral-500 dark:text-neutral-500 
+        ${variant === "mobile" ? "mt-12 sm:hidden" : "mt-5.5"}
+        `}
+    >
+      © Hireme By{" "}
+      <Link href="https://makenedev.vercel.app">
+        <Button variant="link" size="xs" className="text-black dark:text-white">
+          Makene
+        </Button>
+      </Link>{" "}
+      | {currentYear}
+    </p>
+  )
+}
