76f0ebdfb0a2c333b41ceedca68501e458a67275
Makene Neto <makeneto17@gmail.com>
2026-08-20 12:47:14 +0100

    style: format code with prettier

--- src/components/Placeholder.tsx
+++ src/components/Placeholder.tsx
@@ -1,6 +1,6 @@
 interface PlaceholderProps {
-  value: string;
-  fallback: string;
+  value: string
+  fallback: string
 }
 
 export default function Placeholder({ value, fallback }: PlaceholderProps) {
@@ -8,5 +8,5 @@ export default function Placeholder({ value, fallback }: PlaceholderProps) {
     <span>{value}</span>
   ) : (
     <span className="text-neutral-400 dark:text-neutral-600">{fallback}</span>
-  );
+  )
 }
